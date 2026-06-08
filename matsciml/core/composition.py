5986540ef3fd0f7b2851f5a0a22baeff2bca0260
Tanvir Jasarat Opurbo <opurbo2512@gmail.com>
2026-06-08 18:04:15 +0600

    Implement CompositionEngine for formula processing
    Add CompositionEngine class to handle chemical formulas

--- matsciml/core/composition.py
+++ matsciml/core/composition.py
@@ -1,1 +1,16 @@
+from pymatgen.core import Composition
+
+class CompositionEngine:
+    def __init__(self,formula):
+        self.formula = formula
+        self._element_ratios = None
+
+    @property
+    def element_ratios(self):
+        if self._element_ratios is None:
+            comp = Composition(self.formula)
+            self._element_ratios = comp.get_el_amt_dict()
+        return self._element_ratios
+    
+    
 
