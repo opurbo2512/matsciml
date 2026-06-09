a150579a5119fa51a4cf2be772d48cd6c58c0063
Tanvir Jasarat Opurbo <opurbo2512@gmail.com>
2026-06-09 22:54:07 +0600

    Enhance Material class with new attributes and methods
    Added new properties for coordination number, bond length, bond length standard deviation, packing fraction, and valence electron count. Updated calculations for ionic character and valence electrons using numpy.

--- matsciml/core/material.py
+++ matsciml/core/material.py
@@ -1,4 +1,4 @@
-from pymatgen.core import Composition
+from pymatgen.core import Composition,Element
 from pymatgen.core import Structure, Lattice
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.analysis.local_env import CrystalNN
@@ -7,6 +7,12 @@ from pymatgen.analysis.bond_valence import BVAnalyzer
 from mp_api.client import MPRester
 from composition import CompositionEngine
 import math
+import numpy as np
+import warnings
+import logging
+
+warnings.filterwarnings("ignore")
+logging.getLogger("pymatgen").setLevel(logging.ERROR)
 
 class Material:
     def __init__(self,input_formula):
@@ -14,17 +20,22 @@ class Material:
         self._structure = None
         self._sga = None
 
+        #general attributes
         self._formula = None
         self._name = None #do this later
         self._elements = None
         self._n_elements = None
         self._source = None
+
+        #composition attributes
         self._composition = None
         self._electronegativity_diff = None
         self._oxidation_states = None # there are some bug, should update later
-        self._valence_electrons = None
+        self._valence_electrons = None #update later
         self._ionic_character = None
         self._crystal_type = None # do this later
+
+        #structure attributes
         self._crystal_system = None
         self._space_group = None
         self._space_group_number = None
@@ -34,8 +45,17 @@ class Material:
         self._n_atoms_unit_cell = None
         self._n_symmetry_ops = None
         self._is_centrosymmetric = None
+
+        #environment attributes
         self._is_polar = None
+        self._coordination_number = None
+        self._bond_length = None
+        self._bond_length_std = None
+        self._packing_fraction = None
         
+        #electronic attributes
+        self._valence_electron_count = None
+
         if self.input_formula is not None:
             self._run_initial()
 
@@ -179,8 +199,12 @@ class Material:
             self._valence_electrons = {}
             for element in list_of_elements:
                 symbol = element.symbol
-                valenc_cofig = element.valence
-                valence_electrons = sum(valenc_cofig)
+                highest_n = max(n for n,_,_ in element.full_electronic_structure)
+                valence_electrons = sum(
+                    occ
+                    for n,_,occ in element.full_electronic_structure
+                    if n == highest_n
+                )
                 self._valence_electrons[symbol] = valence_electrons
 
         return self._valence_electrons
@@ -189,7 +213,7 @@ class Material:
     def ionic_character(self):
         if self._ionic_character is None:
             en_diff = self.electronegativity_diff
-            self._ionic_character = (1-math.exp(-((en_diff ** 2)/4))) * 100
+            self._ionic_character = (1-np.exp(-((en_diff ** 2)/4))) * 100
 
         return round(self._ionic_character,4)
     
@@ -289,4 +313,95 @@ class Material:
             if point_group_sym in polar_point_group:
                 self._is_polar = True
         return self._is_polar
+    
+    @property
+    def coordination_number(self):
+        if self._coordination_number is None:
+            cnn = CrystalNN()
+            coordination_numbers = []
+
+            for i in range(len(self._structure)):
+                cn = cnn.get_cn(self._structure , i)
+                coordination_numbers.append(cn)
+            
+            self._coordination_number = np.mean(coordination_numbers)
+
+        return round(self._coordination_number,4)
+    
+    @property
+    def bond_length(self):
+        if self._bond_length is None:
+            all_distance = []
+            cnn = CrystalNN()
+            
+            for i in range(len(self._structure)):
+                nn_info = cnn.get_nn_info(self._structure , i)
+
+                for neighbor in nn_info:
+                    dist = self._structure[i].distance(neighbor["site"])
+                    all_distance.append(dist)
+            
+            self._bond_length = np.mean(all_distance)
+
+        return round(self._bond_length,4)
+    
+    @property
+    def bond_length_std(self):
+        if self._bond_length_std is None:
+            cnn = CrystalNN()
+            site_std = []
+
+            for i in range(len(self._structure)):
+                nn_info = cnn.get_nn_info(self._structure , i)
+                local_distances = []
+
+                for neighbor in nn_info:
+                    dist = self._structure[i].distance(neighbor["site"])
+                    local_distances.append(dist)
+
+                if local_distances:
+                    site_std.append(np.std(local_distances))
+
+            self._bond_length_std = np.mean(site_std)
+        return round(self._bond_length_std,4)
+    
+    @property
+    def packing_fraction(self):
+        if self._packing_fraction is None:
+
+            def volume(r):
+                return (4.0/3.0) * (np.pi) * (r ** 3)
+            
+            atomic_volume = 0.0
+            for site in self._structure:
+                radius = site.specie.atomic_radius
+                if radius is None:
+                    radius = site.specie.covalent_radius
+                atomic_volume += volume(radius)
+            cell_volume = self.cell_volume
+            self._packing_fraction = atomic_volume / cell_volume
+
+        return round(self._packing_fraction,4)
+    
+    @property
+    def valence_electron_count(self):
+        if self._valence_electron_count is None:
+            val_electrones = 0.0
+            el_atm_dic = self._structure.composition.get_el_amt_dict()
+            total_atoms = self._structure.composition.num_atoms
+
+            for symbol,amount in el_atm_dic.items():
+                element = Element(symbol)
+                highest_n = max(n for n,_,_ in element.full_electronic_structure)
+                val_ele =sum(
+                    occ
+                    for n,_,occ in element.full_electronic_structure
+                    if n == highest_n
+                )
+                val_electrones += val_ele * amount
+            
+            self._valence_electron_count = val_electrones / total_atoms
+        return round(self._valence_electron_count,4)
+            
+
     
