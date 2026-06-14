dbbffaec7b9e47efe3b03276151dcc7af8dbb93f
Tanvir Jasarat Opurbo <opurbo2512@gmail.com>
2026-06-14 15:38:37 +0600

    Correct method names and enhance featurizers
    Fix method name and add transform method to CompositionFeaturizer class. Adjust symmetry array size in SymmetryFeaturizer.

--- matsciml/features/featurizer.py
+++ matsciml/features/featurizer.py
@@ -8,7 +8,7 @@ class LatticeFeaturizer:
     def __init__(self):
         self._lattice_info = []
 
-    def tansform(self,materials):
+    def transform(self,materials):
         for material in materials:
             a = material.a
             b = material.b
@@ -61,27 +61,53 @@ class BondFeaturizer:
 #Family C — Compositional & Electronic Features
 
 class CompositionFeaturizer:
-    pass
+    def __init__(self):
+        self._info = []
+
+    def transform(self,materials):
+        for material in materials:
+            electronegativity_difference = material.electronegativity_diff
+            electronegativity_mean = material.electronegativity_mean
+            valence_electro_per_atom = material.valence_electron_count
+            d_electron_per_atom = material.d_electron_per_atom
+            avg_ionization_energy = material.avg_ionization_energy
+            avg_electron_affinity = material.avg_electron_affinity
+            cation_to_anion_ratio = material.cation_to_anion_ratio
+            ionic_character = material.ionic_character
+            avg_atomic_mass = material.avg_atomic_mass
+            atomic_number_variance = material.atomic_number_variance
+
+            info_list = [
+                electronegativity_difference,electronegativity_mean,valence_electro_per_atom,d_electron_per_atom,
+                avg_ionization_energy,avg_electron_affinity,cation_to_anion_ratio,
+                ionic_character,avg_atomic_mass,atomic_number_variance
+            ]
+            self._info.append(info_list)
+
+        self._info = np.array(self._info)
+        return self._info
 
 
 #Family D — Symmetry & Topology Features
 class SymmetryFeaturizer:
     def __init__(self):
         self._info = []
-        self.symmetry_list = ["cubic","hexagonal","trigonal","tetragonal","orthorhombic","monoclinic","triclinic"]
+        self.symmetry_list = ["cubic","hexagonal","trigonal","tetragonal","orthorhombic","monoclinic"]
         
     def transform(self,materials):
         for material in materials:
-            zero_array = np.zeros(7)
+            zero_array = np.zeros(6)
             crystal_type = material.crystal_system
-            index_cubic = self.symmetry_list.index(crystal_type)
-            zero_array[index_cubic] = 1
+
+            try:
+                index_cubic = self.symmetry_list.index(crystal_type)
+                zero_array[index_cubic] = 1
+            except:
+                pass
+
             self._info.append(zero_array)
         
         return self._info
     
-        
-    
-
 
 
